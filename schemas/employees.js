5783ee9e48da3ef828cc5f285e24ea831087ec32
DjFIREED <porebskifilip@wp.pl>
2023-03-25 14:25:14 +0100

    employees

---
+++ schemas/employees.js
@@ -0,0 +1,52 @@
+export default {
+  name: 'employees',
+  title: 'Employees',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'phone',
+      title: 'Phone',
+      type: 'string',
+    },
+    {
+      name: 'role',
+      title: 'Role',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+    {
+      name: 'bio',
+      title: 'Bio',
+      type: 'text',
+    },
+    {
+      name: 'fblink',
+      title: 'Facebook Link',
+      type: 'string',
+    },
+    {
+      name: 'instalink',
+      title: 'Instagram Link',
+      type: 'string',
+    },
+    {
+      name: 'twitterlink',
+      title: 'Twitter Link',
+      type: 'string',
+    },
+  ],
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,6 @@ import workinghour from './workinghour'
 import ourservices from './ourservices'
 import service from './service'
 import reviews from './reviews'
+import employees from './employees'
 
-export const schemaTypes = [workinghour, service, ourservices, reviews]
+export const schemaTypes = [workinghour, service, ourservices, reviews, employees]
